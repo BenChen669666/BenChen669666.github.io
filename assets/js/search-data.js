ddcc28e354b1c0d3908ba8ec3852d653ffbe4335
BenChen669666 <47983060+BenChen669666@users.noreply.github.com>
2026-02-08 03:15:32 +0000

    Deploying to gh-pages from @ BenChen669666/BenChen669666.github.io@ea947a0bd35c6d78482873d7ae0b197ef08c49dd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "post-summary-of-allen-zhu-39-s-icml-2024-tutorial-on-physics-of-large-language-models",
         
           title: "Summary of Allen-Zhu&#39;s ICML 2024 Tutorial on Physics of Large Language Models",
